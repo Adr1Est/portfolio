5b1116546e11abf8a6d66bcf90394e97bff3d825
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 12:48:22 +0200

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   if(!isPressed){
     return (
       <div className='flex flex-col gap-3 justify-center items-center'>
-        <p className='text-4xl flex'>Welcome to <span className='username ms-2 me-2'><Typewriter text="Adr1Est's" speed={100}/></span> portfolio</p>
+        <p className='text-4xl flex flex-col md:flex-row text-center'>Welcome to <span className='username ms-2 me-2'><Typewriter text="Adr1Est's" speed={100}/></span> portfolio</p>
         <StartButton text="Dive in" handleClick={handleClick}/>
       </div>
     )
