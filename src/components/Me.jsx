c28d724a28bda665e5a3646d0085698d40dc8a98
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 12:24:54 +0200

    clean up

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -3,11 +3,11 @@ import me from '../assets/processed_image.png'
 
 function Me(){
   return (
-    <div className='flex flex-row items-end gap-3'>
-      <div className='w-50 aspect-square rounded-full overflow-hidden photo-box'>
+    <div className='flex flex-col md:flex-row justify-center items-center gap-3'>
+      <div className='w-25 md:w-50 aspect-square rounded-full overflow-hidden photo-box'>
         <img src={me} alt="adrian portfolio image" className='w-full h-full object-cover' />
       </div>
-      <div className='mb-1'>
+      <div className='mb-1 text-center md:text-start'>
         <p className='text-2xl'>Adrián Estévez Salamanca</p>
         <p className='text-xl'>Full Stack Developer</p>
       </div>
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,31 +3,13 @@ import Marquee from 'react-fast-marquee'
 
 function Stack(){
   const iconClass = `toolIcon transition-all duration-200`
+  const myStack = ['html', 'css', 'javascript', 'react', 'redux', 'vite', 'tailwind', 'bootstrap', 'python', 'flask', 'sqlalchemy', 'nodejs', 'express', 'api', 'git', 'github', 'netlify', 'vercel', 'render', 'docker']
 
   return (
-    <div className='flex flex-col gap-3'>
+    <div className='flex flex-col w-full gap-3'>
       <p className='text-xl'>Stack</p>
-      <Marquee className='flex flex-row gap-3 max-w-150 overflow-y-auto p-2 stack-container' pauseOnHover>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=html" alt="html icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=css" alt="css icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=javascript" alt="javascript icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=react" alt="react icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=redux" alt="redux icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=vite" alt="vite icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=tailwind" alt="vite icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=bootstrap" alt="bootstrap icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=python" alt="python icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=flask" alt="flask icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=sqlalchemy" alt="sqlalchemy icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=nodejs" alt="nodejs icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=express" alt="express icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=api" alt="api icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=git" alt="git icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=github" alt="github icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=netlify" alt="netlify icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=vercel" alt="vercel icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=render" alt="render icon" className={iconClass}/>
-        <img src="https://go-skill-icons.vercel.app/api/icons?i=docker" alt="docker icon" className={iconClass}/>
+      <Marquee className='stack-container' pauseOnHover>
+        {myStack.map(tool => <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`}alt={`${tool} icon`} className={iconClass}/>)}
       </Marquee>
     </div>
   )
