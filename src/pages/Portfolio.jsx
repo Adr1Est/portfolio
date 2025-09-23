66d0d37ab95741bd056556d9bf8199fb9039dbe8
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 13:48:01 +0200

    portfolio spring animation

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Me from '../components/Me'
 import Stack from '../components/Stack'
 import Loader from '../components/Loader'
+import * as motion from 'motion/react-client'
 
 function Portfolio(){
   const [isLoaded, setIsLoaded] = useState()
@@ -22,12 +23,20 @@ function Portfolio(){
     )
   }
   return (
-    <div className='flex flex-col gap-1 rounded-2xl w-full lg:w-200 md:w-150 p-3 main-container'>
+    <motion.div 
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.4 },
+      }}
+      className='flex flex-col gap-1 rounded-2xl w-full lg:w-200 md:w-150 p-3 main-container'
+    >
       <Me/>
       <hr className='mt-3 mb-3 rounded-2xl separator'/>
       <Stack/>
       <hr className='mt-3 mb-3 rounded-2xl separator'/>
-    </div>
+    </motion.div>
   )
 }
 
