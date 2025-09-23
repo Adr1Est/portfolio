61cd3e669dce78f88737eff2fe7ddc6550d846f0
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 13:26:54 +0200

    loader

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react'
 import Me from '../components/Me'
 import Stack from '../components/Stack'
+import Loader from '../components/Loader'
 
 function Portfolio(){
+  const [isLoaded, setIsLoaded] = useState()
+  
+  useEffect(() => {
+    if(!isLoaded){
+      setTimeout(() => {
+        setIsLoaded(true)
+      }, 2000)
+    }
+  }, [isLoaded])
+
+  if(!isLoaded){
+    return (
+      <div className='flex flex-col items-center justify-center gap-1 rounded-2xl w-full lg:w-200 md:w-150 p-3'>
+        <Loader text="Hi"/>
+      </div>
+    )
+  }
   return (
     <div className='flex flex-col gap-1 rounded-2xl w-full lg:w-200 md:w-150 p-3 main-container'>
       <Me/>
