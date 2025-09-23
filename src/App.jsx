727882605819cb2121020927fa17ca9eaa538f27
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 11:52:38 +0200

    fix typewriter and start button

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Me from './components/Me'
 import Stack from './components/Stack'
+import Typewriter from './components/Typewriter.jsx'
+import StartButton from './components/startButton.jsx'
 
 function App() {
   const [isPressed, setIsPressed] = useState(true)
 
   if(isPressed){
     return (
-      <div>
-        <p className='text-4xl'>Welcome to <span className='username'>Adr1Est's</span> portfolio</p>
+      <div className='flex flex-col gap-1 justify-center items-center'>
+        <p className='text-4xl flex'>Welcome to <span className='username ms-2 me-2'><Typewriter text="Adr1Est's" speed={100}/></span> portfolio</p>
+        <StartButton />
       </div>
     )
   }else{
