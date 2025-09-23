991d580a21bb6783b04748fb4a2f9869cf6cf443
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-23 13:48:28 +0200

    stack fix

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -6,7 +6,7 @@ function Stack(){
   const myStack = ['html', 'css', 'javascript', 'react', 'redux', 'vite', 'tailwind', 'bootstrap', 'python', 'flask', 'sqlalchemy', 'nodejs', 'express', 'api', 'git', 'github', 'netlify', 'vercel', 'render', 'docker']
 
   return (
-    <div className='flex flex-col gap-3 w-full'>
+    <div className='flex flex-col gap-3 w-full overflow-hidden'>
       <p className='text-xl'>Stack</p>
       <Marquee className='stack-container' pauseOnHover autoFill>
         {myStack.map(tool => <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`}alt={`${tool} icon`} className={iconClass} title={tool}/>)}
