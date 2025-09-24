a3dc8ab029d3fd8f8b7dd9203c12055ff458d7a4
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-09-24 15:32:17 +0200

    new projects to data

--- src/components/StackAndButtons.jsx
+++ src/components/StackAndButtons.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router'
 import './StackAndButtons.css'
 import Marquee from 'react-fast-marquee'
+import { motion } from "motion/react"
 
 function StackAndButtons(){
   const navigate = useNavigate()
@@ -9,17 +10,27 @@ function StackAndButtons(){
 
   return (
     <>
-      <div className='flex flex-col gap-3 w-full overflow-hidden'>
-        <p className='text-xl'>Stack</p>
-        <Marquee className='stack-container' pauseOnHover autoFill>
-          {myStack.map(tool => <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`}alt={`${tool} icon`} className={iconClass} title={tool}/>)}
-        </Marquee>
-      </div>
-      <hr className='mt-3 mb-3 rounded-2xl separator'/>
-      <div className='flex flex-row gap-3 justify-center items-center'>
-        <button className='rounded-full p-2 portfolioButtons' onClick={() => navigate("/portfolio/projects")}>Projects</button>
-        <button className='rounded-full p-2 portfolioButtons' onClick={() => navigate("/portfolio/aboutme")}>About me</button>
-      </div>
+      <motion.div 
+        className='w-full'
+        initial={{ height: 0 }}
+        animate={{ height: 160 }}
+        transition={{
+          duration: 0.7,
+          scale: { type: "tween", visualDuration: 0.7, bounce: 0.4 },
+        }}
+      >
+        <div className='flex flex-col gap-3 w-full overflow-hidden'>
+          <p className='text-xl'>Stack</p>
+          <Marquee className='stack-container' pauseOnHover autoFill>
+            {myStack.map(tool => <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`}alt={`${tool} icon`} className={iconClass} title={tool}/>)}
+          </Marquee>
+        </div>
+        <hr className='mt-3 mb-3 rounded-2xl separator'/>
+        <div className='flex flex-row gap-3 justify-center items-center'>
+          <button className='rounded-full p-2 portfolioButtons' onClick={() => navigate("/portfolio/projects")}>Projects</button>
+          <button className='rounded-full p-2 portfolioButtons' onClick={() => navigate("/portfolio/aboutme")}>About me</button>
+        </div>
+      </motion.div>
     </>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,31 +1,35 @@
 import './Projects.css'
 import { projectsData } from '../data/projectsData'
-import Marquee from 'react-fast-marquee'
-import { Github } from 'lucide-react'
 import Tilt from 'react-parallax-tilt'
+import ProjectCard from '../components/ProjectCard'
+import { ArrowBigLeft } from 'lucide-react'
+import { useNavigate } from 'react-router'
+import { motion } from "motion/react"
 
 function Projects(){
+  const navigate = useNavigate()
+
   return (
-    <div className="flex flex-row gap-3 w-full overflow-x-auto projects-container">
-      <Tilt tiltReverse={true}>
-        <div className="flex flex-col gap-1 w-70 h-70 rounded-2xl project-card">
-          <div className='w-full h-40 cursor-pointer'>
-            <img src={projectsData[0].img} alt={`${projectsData[0].name} project image`} className='w-full h-full object-cover'/>
-          </div>
-          <div className='flex flex-col gap-1 p-1'>
-            <p className='text-lg'>{projectsData[0].name}</p>
-            <Marquee className='mt-1 tools' autoFill>
-              {projectsData[0].tools.map(tool => {
-                return <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`} alt={`${tool} icon`} className='projectTools' title={tool}/>
-              })}
-            </Marquee>
-            <div className='flex flex-row justify-center items-center mt-3'>
-              <a href={projectsData[0].repo}><Github size={20} className='repoLink'/></a>
-            </div>
-          </div>
-        </div>
-      </Tilt>
-    </div>
+    <>
+      <button className='backIcon' onClick={() => navigate('/portfolio')}>
+        <ArrowBigLeft/>
+      </button>
+      <motion.div 
+        className="flex flex-row gap-3 w-full overflow-x-auto overflow-y-hidden projects-container"
+        initial={{ height: 0 }}
+        animate={{ height: 310 }}
+        transition={{
+          duration: 0.7,
+          scale: { type: "tween", visualDuration: 0.7, bounce: 0.4 },
+        }}
+      >
+        {projectsData.map((project) => (
+          <Tilt tiltReverse={true}>
+            <ProjectCard project={project}/>
+          </Tilt>)
+        )}
+      </motion.div>
+    </>
   )
 }
 
