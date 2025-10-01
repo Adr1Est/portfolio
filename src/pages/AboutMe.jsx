c9809e0d6d60c8f26b7e7b750e7271ed0025527f
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-01 13:55:29 +0200

    glare and glass effect

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,8 @@
+import './AboutMe.css'
 import { motion } from "motion/react"
 import { getAge } from "../data/age"
 import BackButton from "../components/BackButton"
+import Tilt from 'react-parallax-tilt'
 
 function AboutMe(){
   const age = getAge()
@@ -16,17 +18,26 @@ function AboutMe(){
           scale: { type: "tween", visualDuration: 0.7, bounce: 0.4 },
         }}
       >
-        <div className="flex flex-col gap-1 w-full aboutme-box">
-          <p className="text-justify">
-            Hola, soy Adrián Estévez Salamanca de {age} años y vivo en Madrid, España.
-          </p>
-          <p className="text-justify">
-            Soy <strong>Desarrollador Full Stack</strong> con experiencia en 
-            <strong> React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong>, <strong>Python</strong> y <strong>SQL</strong>. 
-            Me apasiona la <strong>innovación tecnológica</strong> y el <strong>aprendizaje constante</strong>, lo que me impulsa 
-            a estar siempre explorando nuevas herramientas.
-          </p>
-        </div>
+        <Tilt
+          tiltEnable={false}
+          glareEnable={true}
+          glareMaxOpacity={0.1}
+          glareColor="white"
+          glarePosition="all"
+          glareBorderRadius="15px"
+        >
+          <div className="flex flex-col gap-1 p-3 w-full aboutme-box">
+            <p className="text-justify">
+              Hola, soy Adrián Estévez Salamanca de {age} años y vivo en Madrid, España.
+            </p>
+            <p className="text-justify">
+              Soy <strong>Desarrollador Full Stack</strong> con experiencia en 
+              <strong> React</strong>, <strong>JavaScript</strong>, <strong>TypeScript</strong>, <strong>Python</strong> y <strong>SQL</strong>. 
+              Me apasiona la <strong>innovación tecnológica</strong> y el <strong>aprendizaje constante</strong>, lo que me impulsa 
+              a estar siempre explorando nuevas herramientas.
+            </p>
+          </div>
+        </Tilt>
       </motion.div>
     </>
   )
