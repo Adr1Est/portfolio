e44155b41540c51ee2529a4c5d7711edd4bc68d8
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-31 16:05:04 +0100

    Welcome css module

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,3 +1,4 @@
+import styles from './Welcome.module.css'
 import { useNavigate } from 'react-router'
 import Typewriter from '../components/Typewriter'
 import StartButton from '../components/StartButton'
@@ -7,7 +8,7 @@ function Welcome(){
 
   return(
     <div className='flex flex-col gap-3 justify-center items-center'>
-      <p className='text-4xl flex flex-col md:flex-row text-center'>Bienvenido al portfolio de <span className='username ms-2 me-2'><Typewriter text="Adr1Est" speed={100}/></span></p>
+      <p className='text-4xl flex flex-col md:flex-row text-center'>Bienvenido al portfolio de <span className={`${styles.username} ms-2 me-2`}><Typewriter text="Adr1Est" speed={100}/></span></p>
       <StartButton text="Ver" handleClick={() => navigate("/portfolio")}/>
     </div>
   )
