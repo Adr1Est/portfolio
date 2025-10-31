dedf94752e7f4932b22a55fc1b3d9bc2222b0f8e
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-10-31 15:45:44 +0100

    ProjectCard css module

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import styles from './ProjectCard.module.css'
 import Marquee from 'react-fast-marquee'
 import { Github } from 'lucide-react'
 import { Project } from '../data/projectsData'
@@ -8,7 +9,7 @@ interface ProjectCardProps {
 
 function ProjectCard({project}: ProjectCardProps){
   return (
-    <div className="flex flex-col gap-1 w-70 h-70 rounded-2xl project-card">
+    <div className={`flex flex-col gap-1 w-70 h-70 rounded-2xl ${styles.projectCard}`}>
       <a href={project.url} target="_blank" rel="noopener noreferrer">
         <div className='w-full h-40 cursor-pointer'>
           <img src={project.img} alt={`${project.name} project image`} className='w-full h-full object-cover'/>
@@ -18,12 +19,12 @@ function ProjectCard({project}: ProjectCardProps){
         <p className='text-lg'>{project.name}</p>
         <Marquee className='mt-1 tools' autoFill>
           {project.tools.map(tool => {
-            return <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`} alt={`${tool} icon`} className='projectTools' title={tool}/>
+            return <img src={`https://go-skill-icons.vercel.app/api/icons?i=${tool}`} alt={`${tool} icon`} className={styles.projectTools} title={tool}/>
           })}
         </Marquee>
         <div className='flex flex-row justify-center items-center mt-3'>
           <a href={project.repo} target="_blank" rel="noopener noreferrer">
-            <Github size={20} className='repoLink'/>
+            <Github size={20} className={styles.repoLink}/>
           </a>
         </div>
       </div>
