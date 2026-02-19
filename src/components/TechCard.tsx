20e16fc6a1d8fef7ea7b5bbaeb1a065001d7a73f
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-02-19 11:02:23 +0100

    TechCard Component

---
+++ src/components/TechCard.tsx
@@ -0,0 +1,26 @@
+import styles from '@/components/TechCard.module.css'
+
+interface Props{
+  logo: string;
+  title: string;
+  level: number;
+}
+
+export default function TechCard({ logo, title, level }: Props){
+  return(
+    <div className={`${styles.techCardMain}`}>
+      <div className='flex flex-row items-center gap-1'>
+        <div>
+          <img src={`https://go-skill-icons.vercel.app/api/icons?i=${logo}`} alt={`logo de ${logo}`} />
+        </div>
+        <h2 className='self-end font-semibold'>{ title }</h2>
+      </div>
+
+      <div className={`${styles.levelContainer} mt-2`}>
+        <div className={`bg-details2 dark:bg-details1 text-white dark:text-first-dark ${styles.levelBar}`} style={{width: `${level}%`}}>
+          {level}%
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -2,6 +2,7 @@ import styles from '@pages/AboutMe.module.css'
 import { motion } from "motion/react"
 import BackButton from "@components/BackButton"
 import Tilt from 'react-parallax-tilt'
+import TechCard from '@/components/TechCard'
 
 function AboutMe(){
   return (
@@ -35,8 +36,23 @@ function AboutMe(){
         <hr className='w-full mt-3 mb-3 rounded-2xl border border-details2 dark:border-details1'/>
 
         <div>
-          <h2 className='text-xl'>Tecnologías</h2>
-          
+          <h2 className='text-xl mb-2'>Stack principal</h2>
+          <div className='flex flex-row justify-center w-full rounded-2xl gap-1 flex-wrap overflow-y-auto h-30'>
+
+            <TechCard logo="typescript" title="TypeScript" level={90}/> 
+            <TechCard logo="react" title="React.js" level={90}/> 
+            <TechCard logo="next" title="Next.js" level={70}/> 
+            <TechCard logo="tailwind" title="Tailwind" level={90}/> 
+            <TechCard logo="css" title="CSS" level={90}/> 
+            <TechCard logo="scss" title="SASS" level={80}/> 
+            <TechCard logo="zustand" title="Zustand" level={60}/> 
+            <TechCard logo="tanstack" title="Tanstack Query" level={100}/> 
+            <TechCard logo="nodejs" title="Node.js" level={80}/> 
+            <TechCard logo="express" title="Express" level={70}/> 
+            <TechCard logo="postgresql" title="PostgreSQL" level={80}/> 
+            <TechCard logo="prisma" title="Prisma" level={80}/> 
+
+          </div>
         </div>
       </motion.div>
     </>
